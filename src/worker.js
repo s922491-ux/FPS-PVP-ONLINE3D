df1738c3dec836dd7d21a868b947510be7ba1dda
s922491-ux <s922491@gunma1.ed.jp>
2026-09-08 13:16:42 +0900

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -25,6 +25,9 @@ export default {
     const url = new URL(request.url);
     if (url.pathname === '/health') return new Response(JSON.stringify({ok:true,service:'fps-pvp',version:'cloudflare'}),{headers:{'content-type':'application/json'}});
     if (url.pathname === '/ws-check') {
+    
+    }
+    
     if (url.pathname === '/do-check') {
   try {
     const id = env.GAME_SERVER.idFromName('debug-check');
