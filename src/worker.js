0dc5226e4a36f898fbb537263bf63c45446f9950
s922491-ux <s922491@gunma1.ed.jp>
2026-09-08 13:32:38 +0900

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -24,14 +24,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname === '/health') return new Response(JSON.stringify({ok:true,service:'fps-pvp',version:'cloudflare'}),{headers:{'content-type':'application/json'}});
-    if (url.pathname === '/ws-check') {
-     return new Response(JSON.stringify({
-        gameServer: !!env.GAME_SERVER
-      }), {
-        headers: { 'content-type': 'application/json' }
-      });
-    } 
-    if (url.pathname === '/do-check') {
+   if (url.pathname === '/do-check') {
   try {
     const id = env.GAME_SERVER.idFromName('debug-check');
     const res = await env.GAME_SERVER.get(id).fetch(
@@ -53,11 +46,6 @@ export default {
     });
   }
 }
-  return new Response(JSON.stringify({
-    gameServer: !!env.GAME_SERVER
-  }), {
-    headers: { 'content-type': 'application/json' }
-  });
 }
     if (request.headers.get('Upgrade') === 'websocket') {
       const id = env.GAME_SERVER.idFromName('global-match-server');
